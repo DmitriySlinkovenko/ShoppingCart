dda2a4b429ad1e2a85dda1efbeeb73aaf6e72381
Dmitriy <dmitriy.slinkovenko@gmail.com>
2025-01-24 14:40:25 -0500

    Shopping car program

--- ShoppingCart/src/Main.java
+++ ShoppingCart/src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		String item;
+		double price;
+		int quantity;
+		char currency = '$';
+		double total;
+		
+		System.out.print("What item would you like to buy?");
+		item = scanner.nextLine();
+		System.out.print("What is the price of the item?");
+		price = scanner.nextDouble();
+		System.out.print("How many items would you like to buy?");
+		quantity = scanner.nextInt();
+		total = price * quantity;
+		
+		
+		System.out.println("You have bought " + quantity + " of " + item + " for " + price+ ". "+ "Your total is " + total + currency);
+		
+		
+		scanner.close();
 	}
-
 }
